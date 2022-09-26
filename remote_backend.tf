648d14986ea202175fc4c7fb1541870d578f11ab
MonseGuzman <andreaguzman.lopez23@gmail.com>
2022-09-26 18:06:37 +0000

    Added the first tag

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "HashiCat-instruqt"
     workspaces {
       name = "hashicat-aws"
