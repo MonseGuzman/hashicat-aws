0ebbe845f286478c37e9d60534cc4a539ae0e1cf
MonseGuzman <andreaguzman.lopez23@gmail.com>
2022-09-26 18:43:06 +0000

    Fix bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,6 @@ module "s3-bucket" {
   source  = "app.terraform.io/HashiCat-instruqt/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket_prefix = "${var.prefix}-s3"
+  bucket_prefix        = var.prefix
+  attach_public_policy = false
 }
